432224da564341fb432ec75f65b85c78daf1e402
Zakaria Abdullahi <zakaria-abdullahi@Zakarias-MacBook-Pro.local>
2023-03-22 17:06:30 -0700

    setup

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -1,26 +1,25 @@
-// frontend/src/index.js
 import React from 'react';
-import './index.css';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import './index.css';
 import App from './App';
 import configureStore from './store';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
 }
 
 function Root() {
   return (
-    <Provider store={store}>
+    <ReduxProvider store={store}>
       <BrowserRouter>
-      <App />
+        <App />
       </BrowserRouter>
-    </Provider>
-  )
+    </ReduxProvider>
+  );
 }
 
 ReactDOM.render(
